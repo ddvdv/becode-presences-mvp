c1108b341df945fbd2bc69fa30d6b3fd1ac1f582
DavidV <davidv@DavidVs-MacBook-Pro.local>
2017-09-29 12:54:09 +0200

    everything but 24

--- app.js
+++ app.js
@@ -39,17 +39,9 @@ $(document).ready(function(){
           <div class="name">
             ${student.fullName}
           </div>
-          <div class="arrival">
-            <div class="btn-group" role="group">
-            <button id="btnGroupDrop1" type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Arrived late
-            </button>
-            <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-            <a class="dropdown-item" href="#" id="arrivedNow">Arrived now</a><br><br>
-            <a class="dropdown-item" href="#" id="arrivedAt">Arrived at...</a>
-            </div>
-            </div>
-            <button class="btn btn-success" id="onTime">On Time</button>
+            <div class="arrival">
+              <button class="btn btn-warning" id="late">late</button>
+              <button class="btn btn-success" id="onTime">on time</button>
             </div>
           </div>
         </div>
@@ -59,6 +51,17 @@ $(document).ready(function(){
 
     moment.locale('fr');  // set up de la librairie locale en français
     let supposedTimeofArrival = '09:30'; // Heure d'arrivée pour le "On Tine"
+
+    // Déploiement des boutons "retard"
+    $(".student").on('click', '#late', function(){
+      let currentStudent = $(this).closest('.student').attr('id');
+      $("#"+currentStudent+" .arrival").html(`
+        <button class="btn btn-warning" id="arrivedNow">Arrived now</button>
+        <button class="btn btn-warning" id="arrivedAt">Arrived at...</button>
+        <button class="btn btn-primary" id="edit">back</button>
+        `);
+    });
+
 
     // Arrivée à temps
     $(".student").on('click', '#onTime', function(){
@@ -90,10 +93,11 @@ $(document).ready(function(){
     // Sous-button arrivée en retard, dirige vers snippet time
     $(".student").on('click', '#arrivedAt', function(){
       let currentStudent = $(this).closest('.student').attr('id');
-      $("#"+currentStudent+" .arrival").html(`        <div class='col-xs-8'>
+      $("#"+currentStudent+" .arrival").html(`
+        <div class='col-xs-12'>
         <div class="form-group">
-        <div class='input-group date' id='datetimepicker3'>
-        <input type='text' class="form-control" />
+        <div class='input-group date' id='datetimepicker'>
+        <input type='text' class="form-control" id="timeOfArrival" />
         <span class="input-group-addon">
         <span class="glyphicon glyphicon-time"></span>
         </span>
@@ -102,37 +106,35 @@ $(document).ready(function(){
         </div>
         <button class="btn btn-primary" id="enterTime">Enter</button>
         `);
-        // et ajout du js snippet time
-        $('#datetimepicker3').datetimepicker({
+        //  ajout du js snippet time
+        $('#datetimepicker').datetimepicker({
           format: 'LT'
         });
-       // et d'un bouton pour valider l'entrée
-       $(".arrival").on('click', '#enterTime', function(){
+        //  simulation du click sur le snippet
+        $( "#datetimepicker .glyphicon" ).trigger( "click" );
+       //  ajour d'un bouton pour valider l'entrée
+        $(".arrival").on('click', '#enterTime', function(){
           let currentStudent = $(this).closest('.student').attr('id');
+          let timeOfArrival = $('#timeOfArrival').val();
+          console.log(timeOfArrival);
+
           $("#"+currentStudent+" .arrival").html(`
-            <p>arrived at <span>09.30</span></p>
+            <p>arrived at <span>${timeOfArrival}</span></p>
             <button class="btn btn-primary" id="edit">Edit</button>
           `);
-      });
+        });
      });
-    $(".student").on('click', 'datetimepicker3', function(){
-      $('#datetimepicker3').datetimepicker({
+    $(".student").on('click', 'datetimepicker', function(){
+      $('#datetimepicker').datetimepicker({
         format: 'LT'
       });
     });
     // Boutton Edit pour rebasculer au statut initial
     $(".student").on('click', '#edit', function(){
       let currentStudent = $(this).closest('.student').attr('id');
-      $("#"+currentStudent+" .arrival").html(`        <div class="btn-group" role="group">
-        <button id="btnGroupDrop1" type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Arrived late
-        </button>
-        <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-        <a class="dropdown-item" href="#" id="arrivedNow">Arrived now</a><br><br>
-        <a class="dropdown-item" href="#" id="arrivedAt">Arrived at...</a>
-        </div>
-        </div>
-        <button class="btn btn-success" id="onTime">On Time</button>
+      $("#"+currentStudent+" .arrival").html(`
+         <button class="btn btn-warning" id="late">late</button>
+         <button class="btn btn-success" id="onTime">on time</button>
         `);
     });
   });
